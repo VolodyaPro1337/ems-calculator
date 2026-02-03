99bfb5ff99665333511056508be3dbe2c933a8ad
VolodyaPro1337 <kuleminson@gmail.com>
2026-02-03 08:50:52 +0300

    feat: add vercel speed insights

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './style.css'
 import App from './App.vue'
 
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 inject()
+injectSpeedInsights()
 
 createApp(App).mount('#app')
